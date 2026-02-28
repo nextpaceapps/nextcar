9b90c7987c2b686259af9f9d94d07c3814040edc
Igor Lashchenko <igor@nextpace.app>
2026-02-28 08:35:20 +0200

    fix(frontend): allow local firebase emulator images on next/image

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -16,6 +16,14 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
       }
     ],
   },
